f3bb9cd29864d4a150ea14456513a5c29b1ab178
hoang-debug <campus199@gmail.com>
2021-10-07 22:11:47 +0700

    bai 1 bai 2

---
+++ Day 1/app.js
@@ -0,0 +1,24 @@
+//Bai 1
+let str = prompt('Nhap chuoi ki tu can ma hoa:')
+let rot13Cipher = function (str){
+    let input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    let index     = x => input.indexOf(x);
+    let translate = x => index(x) > -1 ? output[index(x)] : x;
+  return alert(str.split('').map(translate).join(''));
+}
+rot13Cipher(str);
+//Bai 3
+function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  translatePigLatin("california");
+//Bai 2
